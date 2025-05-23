dac177d33441c9e4b668f3e8548c2e4be89698de
Saif <saifnassermahmoud@gmail.com>
2025-05-24 00:31:33 +0300

    x test

---
+++ models/index.js
@@ -0,0 +1,19 @@
+const Admin = require('./Admin');
+const Client = require('./Client');
+const Invoice = require('./Invoice');
+const InvoiceItem = require('./InvoiceItem');
+const Report = require('./Report');
+const ReportTechnicalTest = require('./ReportTechnicalTest');
+const InvoiceReport = require('./invoicereport'); 
+const Login = require('./login');
+
+module.exports = {
+  Admin,
+  Client,
+  Invoice,
+  InvoiceItem,
+  Report,
+  ReportTechnicalTest,
+  InvoiceReport, 
+  Login,
+};
