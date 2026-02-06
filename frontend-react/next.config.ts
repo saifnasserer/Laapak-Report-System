551b08d36d9c35fa5a710b4e58f0b124e2b2e62e
Saifnasserer <saifnassermahmoud@gmail.com>
2026-02-06 20:10:40 +0200

    chore: disable eslint and typecheck during build to save memory

--- frontend-react/next.config.ts
+++ frontend-react/next.config.ts
@@ -5,6 +5,17 @@ const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
   async rewrites() {
     return [
       {
