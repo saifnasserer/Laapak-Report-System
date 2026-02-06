840d392843dac40f422d098e2e45e4663d60bfa1
Saifnasserer <saifnassermahmoud@gmail.com>
2026-02-06 21:42:58 +0200

    chore(frontend): disable type check and lint during build to save memory

--- frontend-react/next.config.ts
+++ frontend-react/next.config.ts
@@ -4,6 +4,12 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   /* config options here */
   async rewrites() {
     return [
